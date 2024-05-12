11a03bad9c9a83beb828165b5336f51e99cfd4b0
masud4657 <masudpfrnt@gmail.com>
2024-05-12 11:27:10 +0600

    added the Our Popular Services section in the Home page.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import PopularServices from "./PopularServices";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <PopularServices></PopularServices>
         </div>
     );
 };
---
+++ src/Pages/Home/PopularServices.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PopularServiceCard from "../../Components/PopularServiceCard";
+
+
+const PopularServices = () => {
+    const [services,setServices]= useState()
+
+    useEffect(()=>{
+        axios.get('http://localhost:6003/addServices')
+        .then(res=>{
+            setServices(res.data)
+        })
+        .catch(error=>{
+            console.error(error);
+        })
+    },[])
+    return (
+        <div>
+            <p className="text-center text-3xl font-bold mb-10">Our Popular Services: {services?.slice(0,6).length}{" "}</p>
+           <div className="grid grid-cols-2 gap-10">
+           {services&&
+            services.slice(0,6).map(service=> <PopularServiceCard key={service._id} service={service}></PopularServiceCard>)
+           }
+           </div>
+        </div>
+    );
+};
+
+export default PopularServices;
