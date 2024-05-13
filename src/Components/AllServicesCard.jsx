5485ee584da6d3006ac933fc1b5007e59a2c6a1e
masud4657 <masudpfrnt@gmail.com>
2024-05-13 20:07:37 +0600

    added the all swrvices page section.

---
+++ src/Components/AllServicesCard.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+
+
+const AllServicesCard = ({service}) => {
+    const {_id,image,name,price,description,providerImage,providerName} = service
+    return (
+        <div>
+        <div className=" flex flex-col justify-between p-6   h-full space-y-6 overflow-hidden  rounded-lg shadow-md dark:bg-gray-50 dark:text-gray-800">
+          <div className="flex space-x-4">
+            <img
+              alt=""
+              src={providerImage}
+              className="object-cover w-12 h-12 rounded-full shadow dark:bg-gray-500"
+            />
+            <div className="flex flex-col space-y-1">
+              <p>{providerName}</p>
+              {/* <a
+                rel="noopener noreferrer"
+                href="#"
+                className="text-sm font-semibold"
+              >
+                Leroy Jenkins
+              </a>
+              <span className="text-xs dark:text-gray-600">4 hours ago</span> */}
+            </div>
+          </div>
+          <div>
+            <img
+              src={image}
+              alt=""
+              className="object-cover w-full mb-4 h-60 sm:h-96 rounded-md dark:bg-gray-500"
+            />
+            <h2 className="mb-1 text-xl font-semibold">
+             {name}
+            </h2>
+            <p className="text-sm dark:text-gray-600">
+              {description}
+            </p>
+            <p>Price: {price}</p>
+          </div>
+          <div> 
+          <Link to={`/viewDetails/${_id}`}><button   className=" btn btn-block btn-primary flex items-center justify-center  p-3 font-semibold  rounded-md dark:bg-violet-600 dark:text-gray-50">View Details</button></Link>
+          </div>
+         
+        </div>
+      </div>
+    );
+};
+
+export default AllServicesCard;
--- src/Pages/AllServices/AllServices.jsx
+++ src/Pages/AllServices/AllServices.jsx
@@ -1,9 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AllServicesCard from "../../Components/AllServicesCard";
 
 
 const AllServices = () => {
+
+    const [services,setServices]= useState()
+
+    useEffect(()=>{
+        axios.get('http://localhost:6003/addServices')
+        .then(res=>{
+            setServices(res.data)
+        })
+        .catch(error=>{
+            console.error(error);
+        })
+    },[])
     return (
         <div>
-            <h1>All Services Page</h1>
+            <p className="text-center text-3xl font-bold mb-10">Our Services:</p>
+           <div className="grid grid-cols-2 gap-10">
+           {services&&
+            services.map(service=> <AllServicesCard key={service._id} service={service}></AllServicesCard>)
+           }
+           </div>
         </div>
     );
 };
